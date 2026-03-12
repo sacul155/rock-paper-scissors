2a9fc08834860511b320f71d952863d0240970e9
sacul155 <cr1sram1rez107@gmail.com>
2026-03-12 18:56:16 -0400

    Added methods to playGame() function that add a new element to display winner and score of each round on webpage.
    Added conditional to restart game if one player's score reaches 5.
    
    This commit gave me some practice creating new elements in js, adding a class to them, adding text content to these
    elements, and appending them inside a parent node.
    
    Also moved button variable initialization and appendage above the game function so that new text elements appear
    under buttons instead of on top.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -13,6 +13,22 @@ let computerChoice = '';
 let humanScore = 0;
 let computerScore = 0;
 
+const rockbtn = document.createElement("button");
+rockbtn.textContent = "Rock";
+document.body.appendChild(rockbtn);
+
+const paperbtn = document.createElement("button");
+paperbtn.textContent = "Paper";
+document.body.appendChild(paperbtn);
+
+const scissorsbtn = document.createElement("button");
+scissorsbtn.textContent = "Scissors";
+document.body.appendChild(scissorsbtn);
+
+const content = document.createElement("div");
+content.classList.add("content");
+document.body.appendChild(content);
+
 function playGame(humanChoice, computerChoice) {
     /* computerChoice is first assigned for comparison; don't need to assign humanChoice b/c it's assigned
     based on which button is clicked */
@@ -27,54 +43,108 @@ function playGame(humanChoice, computerChoice) {
     Also updates score variables based on winner and prints to console as well. */
     if (humanChoice === computerChoice) {
         console.log("tie");
+        const tieMessage = document.createElement("div");
+        tieMessage.classList.add("subContent");
+        tieMessage.textContent = "TIE";
+        content.appendChild(tieMessage);
         console.log("Your score: " + humanScore + ", Computer score: " + computerScore);
+        const scoreUpdate = document.createElement("div");
+        scoreUpdate.classList.add("subContent");
+        scoreUpdate.textContent = "SCORE: PLAYER HAS " + humanScore + ", COMPUTER HAS " + computerScore;
+        content.appendChild(scoreUpdate);
     } else if (humanChoice === "rock") {
         if (computerChoice === "paper") {
             console.log("You lose");
+            const loseMessage = document.createElement("div");
+            loseMessage.classList.add("subContent");
+            loseMessage.textContent = "YOU LOSE";
+            content.appendChild(loseMessage);
             computerScore += 1;
             console.log("Your score: " + humanScore + ", Computer score: " + computerScore);
+            const scoreUpdate = document.createElement("div");
+            scoreUpdate.classList.add("subContent");
+            scoreUpdate.textContent = "SCORE: PLAYER HAS " + humanScore + ", COMPUTER HAS " + computerScore;
+            content.appendChild(scoreUpdate);
         } else if (computerChoice === "scissors") {
             console.log("You win");
+            const winMessage = document.createElement("div");
+            winMessage.classList.add("subContent");
+            winMessage.textContent = "YOU WIN";
+            content.appendChild(winMessage);
             humanScore += 1;
             console.log("Your score: " + humanScore + ", Computer score: " + computerScore);
+            const scoreUpdate = document.createElement("div");
+            scoreUpdate.classList.add("subContent");
+            scoreUpdate.textContent = "SCORE: PLAYER HAS " + humanScore + ", COMPUTER HAS " + computerScore;
+            content.appendChild(scoreUpdate);
         }
     } else if (humanChoice === "paper") {
         if (computerChoice === "rock") {
             console.log("You win");
+            const winMessage = document.createElement("div");
+            winMessage.classList.add("subContent");
+            winMessage.textContent = "YOU WIN";
+            content.appendChild(winMessage);
             humanScore += 1;
             console.log("Your score: " + humanScore + ", Computer score: " + computerScore);
+            const scoreUpdate = document.createElement("div");
+            scoreUpdate.classList.add("subContent");
+            scoreUpdate.textContent = "SCORE: PLAYER HAS " + humanScore + ", COMPUTER HAS " + computerScore;
+            content.appendChild(scoreUpdate);
         } else if (computerChoice === "scissors") {
             console.log("You lose");
+            const loseMessage = document.createElement("div");
+            loseMessage.classList.add("subContent");
+            loseMessage.textContent = "YOU LOSE";
+            content.appendChild(loseMessage);
             computerScore += 1;
             console.log("Your score: " + humanScore + ", Computer score: " + computerScore);
+            const scoreUpdate = document.createElement("div");
+            scoreUpdate.classList.add("subContent");
+            scoreUpdate.textContent = "SCORE: PLAYER HAS " + humanScore + ", COMPUTER HAS " + computerScore;
+            content.appendChild(scoreUpdate);
         }
     } else if (humanChoice === "scissors") {
         if (computerChoice === "rock") {
             console.log("You lose");
+            const loseMessage = document.createElement("div");
+            loseMessage.classList.add("subContent");
+            loseMessage.textContent = "YOU LOSE";
+            content.appendChild(loseMessage);
             computerScore += 1;
             console.log("Your score: " + humanScore + ", Computer score: " + computerScore);
+            const scoreUpdate = document.createElement("div");
+            scoreUpdate.classList.add("subContent");
+            scoreUpdate.textContent = "SCORE: PLAYER HAS " + humanScore + ", COMPUTER HAS " + computerScore;
+            content.appendChild(scoreUpdate);
         } else if (computerChoice === "paper") {
             console.log("You win");
+            const winMessage = document.createElement("div");
+            winMessage.classList.add("subContent");
+            winMessage.textContent = "YOU WIN";
+            content.appendChild(winMessage);
             humanScore += 1;
             console.log("Your score: " + humanScore + ", Computer score: " + computerScore);
+            const scoreUpdate = document.createElement("div");
+            scoreUpdate.classList.add("subContent");
+            scoreUpdate.textContent = "SCORE: PLAYER HAS " + humanScore + ", COMPUTER HAS " + computerScore;
+            content.appendChild(scoreUpdate);
         }
      } else console.log("No contest");
+     if (humanScore >= 5 || computerScore >= 5) {
+        humanScore = 0;
+        computerScore = 0;
+        const endGame = document.createElement("div");
+        endGame.classList.add("subContent");
+        endGame.textContent = "GAME OVER: START NEW GAME";
+        content.appendChild(endGame);
+     }
 }
 
 /* Now that the function has been written, we need a way to call the function on command.  The following buttons
 are created and appended to HTML doc, then event listeners are added which call the playGame() function each time
 they are clicked. */
-const rockbtn = document.createElement("button");
-rockbtn.textContent = "Rock";
-document.body.appendChild(rockbtn);
+
 rockbtn.addEventListener("click", () => {playGame("rock", computerChoice)});
-
-const paperbtn = document.createElement("button");
-paperbtn.textContent = "Paper";
-document.body.appendChild(paperbtn);
 paperbtn.addEventListener("click", () => {playGame("paper", computerChoice)});
-
-const scissorsbtn = document.createElement("button");
-scissorsbtn.textContent = "Scissors";
-document.body.appendChild(scissorsbtn);
 scissorsbtn.addEventListener("click", () => {playGame("scissors", computerChoice)});
